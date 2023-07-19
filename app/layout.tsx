5a5db1f95859057014736abf0697b36c91cbe976
VikramAditya <deepkumarkhamaru@gmail.com>
2023-07-19 17:07:01 +0530

    prettified

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,26 @@
-import './globals.css'
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import "./globals.css";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Shunya',
-  description: 'Awesome todo app that has never been done',
-}
+  title: "Shunya",
+  description: "Awesome todo app that has never been done",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} bg-slate-800 px-5 text-slate-100 flex flex-row justify-center items-center`}>{children}</body>
+      <body
+        className={`${inter.className} bg-slate-800 px-5 text-slate-100 flex flex-row justify-center items-center`}
+      >
+        {children}
+      </body>
     </html>
-  )
+  );
 }
